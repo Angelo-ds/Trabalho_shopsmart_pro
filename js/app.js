32ce06535342f67a219553f99c8f94da20e8c367
Angelo de Barros Ribeiro <angelo.b.ribeiro@aluno.senai.br>
2026-02-20 07:55:07 -0300

    Corrção1: Estrutura de Erro repetida nas mensagens de erro

--- js/app.js
+++ js/app.js
@@ -3,6 +3,19 @@ const verificaV1 = document.getElementById('valor01');
 const verificaV2 = document.getElementById('valor02'); 
 const resultado = document.getElementById('resultado');
 
+function mostrarErro() {
+    resultado.style.display = 'flex';
+    resultado.style.background = 'red';
+    resultado.innerHTML = `
+    <ul>
+        <li>Erro</li>
+        <li>Insira um valor válido para os dois campos.</li>
+    </ul>
+    `;
+
+    verificaV1.focus();
+}
+
 function calcularTotal(){
 
     const v1 = parseFloat(verificaV1.value);
@@ -10,25 +23,19 @@ function calcularTotal(){
 
 
     if (isNaN(v1) || isNaN(v2)){
-        resultado.innerHTML=
-        `
-        <h2>Erro<h2>
-        <p>insira um valor válido para os dois campos.<p>
-        `
-        resultado.style.background ="red"
-        verificaV1.focus();
-        
+        mostrarErro();
         return;
     }
 
     resultado.style.display = 'flex';
+    resultado.style.background = 'lightblue';
 
     resultado.innerHTML = `
-        <ul>
+
             <h2>Total da compra<h2>
             <p>Houve uma compra de ${v2} unidades com o preço unitário de R$${v1}
             <p>Seu total esperado é: R$${v1 * v2}</p>
-        </ul>
+
     `;
 }
 
@@ -41,25 +48,21 @@ function desconto(){
     let  v3 = (v1 * v2) /100
 
     if (isNaN(v1) || isNaN(v2)){
-        resultado.innerHTML=
-        `
-        <h2>Erro<h2>
-        <p>insira um valor válido para os dois campos.<p>
-        `
-        resultado.style.background ="red"
-        verificaV1.focus();
+
+        mostrarErro();
         return;
     }
 
 
     resultado.style.display = 'flex';
+    resultado.style.background = 'lightblue';
 
     resultado.innerHTML = `
-        <ul>
+
             <h2>Desconto esperado<h2>
             <p>foi aplicado um desconto de R$${v2} sobre o valor de R$${v1}
             <p>Seu desconto esperado é: R$${v1 - v3}<p>
-        </ul>
+
     `;
 }
 
@@ -71,25 +74,20 @@ function juros(){
     let v3 = (v1 * v2) /100
 
     if (isNaN(v1) || isNaN(v2)){
-        resultado.innerHTML=
-        `
-        <h2>Erro<h2>
-        <p>insira um valor válido para os dois campos.<p>
-        `
-        resultado.style.background ="red"
-        verificaV1.focus();
+
+        mostrarErro();
         return;
     }
 
 
     resultado.style.display = 'flex';
-
+    resultado.style.background = 'lightblue';
     resultado.innerHTML = `
-        <ul>
+
             <h2>Juros da compra<h2>
             <p>foi aplicado um acréscimo de R$${v2} sobre o valor de R$${v1}
             <p>Seu Juros esperado é: R$${v1 + v3}<p>
-        </ul>
+
     `;
 }
 
@@ -100,25 +98,21 @@ function comissao(){
     let v3 = v2 /100
 
     if (isNaN(v1) || isNaN(v2)){
-        resultado.innerHTML=
-        `
-        <h2>Erro<h2>
-        <p>insira um valor válido para os dois campos.<p>
-        `
-        resultado.style.background ="red"
-        verificaV1.focus();
+
+        mostrarErro();
         return;
     }
 
 
     resultado.style.display = 'flex';
+    resultado.style.background = 'lightblue';
 
     resultado.innerHTML = `
-        <ul>
+
             <h2>Comissão da compra<h2>
             <p>Foi aplicado uma porcentagem de ${v2}% sobre o valor de R$${v1}
             <p>Sua comissão esperada é: R$${v1 * v3}</p>
-        </ul>
+
     `;
 }
 
@@ -128,25 +122,21 @@ function lucro(){
     const v2 = parseFloat(verificaV2.value);
 
     if (isNaN(v1) || isNaN(v2)){
-        resultado.innerHTML=
-        `
-        <h2>Erro<h2>
-        <p>insira um valor válido para os dois campos.<p>
-        `
-        resultado.style.background ="red"
-        verificaV1.focus();
+
+        mostrarErro();
         return;
     }
 
 
     resultado.style.display = 'flex';
+    resultado.style.background = 'lightblue';
 
     resultado.innerHTML = `
-        <ul>
+
             <h2>Lucro da compra<h2>
             <p>Teve um gasto de R$${v2} sobre o valor de R$${v1}
             <p>Seu lucro esperado é: R$${v1 - v2}<p>
-        </ul>
+
     `;
 }
 
